032e067c238847b141bcd785861845b6664ee057
Sumit Singh Rajput <itssumit258@gmail.com>
2023-03-06 23:06:29 +0530

    working on store

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,7 +3,7 @@
 import { RootState } from "@/store/store";
 import { useDispatch, useSelector } from "react-redux";
 import Image from "next/image";
-import { MinusIcon, PlusIcon } from "@heroicons/react/24/solid";
+import { MinusIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/solid";
 import { add, sub } from "@/store/cartSlice";
 
 function cart() {
@@ -11,13 +11,13 @@ function cart() {
   const dispatch = useDispatch();
   console.log("product:", product);
   return (
-    <section className="w-screen bg-[#f7f7f5] h-screen">
-      <div className="flex flex-col justify-center items-center w-full p-1 gap-5">
+    <section className="w-screen bg-[#f7f7f5] h-[100svh]">
+      <div className="flex flex-col justify-center items-center w-[100svh] gap-5">
         {product.map((item, key) => {
           return (
             <div
               key={key}
-              className="w-full border grid grid-cols-5 border-gray-300 gap-5 md:gap-5 bg-white rounded-lg"
+              className="w-full p-2 h-auto border flex flex-col justify-center items-center md:grid grid-cols-5 border-gray-300 gap-5 bg-white rounded-lg "
             >
               <div className="py-2 px-2 flex justify-center items-center">
                 <Image
@@ -27,31 +27,29 @@ function cart() {
                   width={100}
                 />
               </div>
-              <div className="flex justify-start items-center ">
+              <div className="flex justify-start items-center text-center">
                 {item.title}
               </div>
               <div className=" flex justify-start items-center">
                 {item.price}
               </div>
               <div className=" flex justify-start items-center gap-2">
-                <div className="bg-blue-700 w-8 h-8 flex justify-center items-center">
+                <div className=" bg-blue-700 w-8 h-8 flex justify-center items-center">
                   <MinusIcon
                     onClick={() => dispatch(sub(item))}
-                    className="w-5 h-5 bg-blue-700 text-white p-0.2"
+                    className="w-5 h-5 text-white"
                   />
                 </div>
                 {item.cartQuantity}
                 <div className="bg-blue-700 w-8 h-8 flex justify-center items-center">
                   <PlusIcon
                     onClick={() => dispatch(add(item))}
-                    className="w-5 h-5   text-white"
+                    className="w-5 h-5 text-white"
                   />
                 </div>
-              </div>
-              <div className="w-full h-full flex justify-center items-center">
-                <button className="w-20 h-10 p-2 bg-red-700 font-bold text-white text-lg rounded-lg text-center">
-                  X
-                </button>
+                <div className="flex justify-center items-center absolute right-10 sm:relative sm:left-10">
+                  <TrashIcon className="w-8 h-8 text-red-700" />
+                </div>
               </div>
             </div>
           );
--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="h-[100svh]">
+      <body className="h-[100svh] w-screen">
         <StoreProvider>
           <NavBar />
           {children}
