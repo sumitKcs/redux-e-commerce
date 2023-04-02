4ac5a95ac014d880fedcaffd7b92d5c4468eaee3
Sumit Singh Rajput <itssumit258@gmail.com>
2023-04-02 09:39:41 +0530

    grid transition effect

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -2,15 +2,25 @@ import React from "react";
 
 const Featured = () => {
   return (
-    <section className="container mx-auto mb-5  feature-grid md:scale-y-[.80] md:scale-x-95">
-      <div className="flex justify-center group  item item-1 ">
-        <div className="relative w-full drop-shadow-xl group-hover:scale-105  transition-transform duration-200 ease-out">
+    <section className="container mx-auto mb-5  feature-grid md:scale-y-[.80] md:scale-x-95 cursor-pointer">
+      {/* cell 1  */}
+      <div className="flex justify-center item-1 overflow-hidden rounded-2xl">
+        <div className="relative w-full drop-shadow-xl transition-transform duration-200 ease-out">
           <img
-            className="w-full h-full rounded-2xl object-cover  "
-            src="https://cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&width=700"
+            className="w-full h-full rounded-2xl object-cover  zoom-in"
+            src="//cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&amp;width=1500"
             alt=""
-          />
-          <div className="absolute top-0  text-white  text-2xl md:text-4xl font-bold ">
+            srcSet="//cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&amp;width=200 200w, //cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&amp;width=300 300w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&amp;width=400 400w, //cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&amp;width=500 500w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&amp;width=600 600w, //cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&amp;width=700 700w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&amp;width=800 800w, //cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&amp;width=1000 1000w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&amp;width=1200 1200w, //cdn.shopify.com/s/files/1/0153/8863/files/VmodaCrossfade2Wireless_1b26e891-eac3-4b66-9f38-becd63f5829b.jpg?v=1676363664&amp;width=1400 1400w"
+            width="1500"
+            height="1500"
+            loading="lazy"
+            sizes="(max-width: 699px) 100vw, min(780px, 50vw)"
+          ></img>
+          <div className="absolute top-0  text-white  text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold ">
             <p className="mt-10 mx-10 flex justify-center items-center  text-center ">
               V-MODA Crossfade 2 Wireless Codex Edition
             </p>
@@ -18,16 +28,30 @@ const Featured = () => {
         </div>
       </div>
 
-      {/* cell 2 */}
+      {/* cell 2 & 3 */}
 
-      <div className=" w-full item item-2 flex  justify-center items-center gap-4 ">
-        <div className="relative  drop-shadow-xl group transition-transform duration-200 ease-out">
+      <div className=" w-full item-2 flex  justify-center items-center gap-4 ">
+        {/* cell 2  */}
+        <div className="relative  drop-shadow-xl  transition-transform duration-200 ease-out overflow-hidden rounded-2xl">
           <img
-            className="w-full h-full rounded-2xl object-cover group-hover:scale-105"
-            src="https://cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&width=400"
+            className="w-full h-full rounded-2xl object-cover zoom-in"
+            src="//cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&amp;width=1500"
             alt=""
-          />
-          <div className="absolute top-0  text-white  text-2xl md:text-4xl font-bold ">
+            srcSet="//cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&amp;width=200 200w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&amp;width=300 300w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&amp;width=400 400w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&amp;width=500 500w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&amp;width=600 600w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&amp;width=700 700w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&amp;width=800 800w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&amp;width=1000 1000w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&amp;width=1200 1200w, //cdn.shopify.com/s/files/1/0153/8863/files/ZE3000.jpg?v=1676367536&amp;width=1400 1400w"
+            width="1500"
+            height="1500"
+            loading="lazy"
+            sizes="(max-width: 699px) 100vw, min(390px, 25vw)"
+          ></img>
+          <div className="absolute top-0  text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold ">
             <p className="mt-10 mx-10 flex justify-center items-center  text-center ">
               Final Ze3000
             </p>
@@ -35,13 +59,27 @@ const Featured = () => {
         </div>
 
         {/* cell 3 */}
-        <div className="relative drop-shadow-xl group  transition-transform duration-200 ease-out">
+        <div className="relative drop-shadow-xl transition-transform duration-200 ease-out overflow-hidden rounded-2xl">
           <img
-            className="w-full h-full rounded-2xl  object-cover group-hover:scale-105"
-            src="https://cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&width=400"
+            className="w-full h-full rounded-2xl object-cover zoom-in"
+            src="//cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&amp;width=1500"
             alt=""
-          />
-          <div className="absolute top-0  text-white  text-2xl md:text-4xl font-bold ">
+            srcSet="//cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&amp;width=200 200w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&amp;width=300 300w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&amp;width=400 400w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&amp;width=500 500w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&amp;width=600 600w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&amp;width=700 700w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&amp;width=800 800w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&amp;width=1000 1000w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&amp;width=1200 1200w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Fiiobtr5.jpg?v=1676369219&amp;width=1400 1400w"
+            width="1500"
+            height="1500"
+            loading="lazy"
+            sizes="(max-width: 699px) 100vw, min(390px, 25vw)"
+          ></img>
+          <div className="absolute top-0 text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold ">
             <p className="mt-10 mx-10 flex justify-center items-center  text-center ">
               FiiO BRT5
             </p>
@@ -50,17 +88,27 @@ const Featured = () => {
       </div>
 
       {/* cell 4  */}
-      <div className="flex justify-center item item-3 group ">
-        <div className="relative w-full drop-shadow-xl group-hover:scale-105   transition-transform duration-200 ease-out">
+      <div className="flex justify-center item-3 overflow-hidden ">
+        <div className="relative w-full drop-shadow-xl overflow-hidden transition-transform duration-200 ease-out rounded-2xl">
           <img
-            className="w-full h-full rounded-2xl object-cover "
-            src="https://cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-Truthear-ZERO-Drop-Banner-01.jpg?v=1675945400&width=700"
+            className="w-full h-full rounded-2xl object-cover zoom-in"
+            src="//cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-Truthear-ZERO-Drop-Banner-01.jpg?v=1675945400&amp;width=1180"
             alt=""
+            srcSet="//cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-Truthear-ZERO-Drop-Banner-01.jpg?v=1675945400&amp;width=200 200w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-Truthear-ZERO-Drop-Banner-01.jpg?v=1675945400&amp;width=300 300w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-Truthear-ZERO-Drop-Banner-01.jpg?v=1675945400&amp;width=400 400w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-Truthear-ZERO-Drop-Banner-01.jpg?v=1675945400&amp;width=500 500w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-Truthear-ZERO-Drop-Banner-01.jpg?v=1675945400&amp;width=600 600w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-Truthear-ZERO-Drop-Banner-01.jpg?v=1675945400&amp;width=700 700w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-Truthear-ZERO-Drop-Banner-01.jpg?v=1675945400&amp;width=800 800w, 
+            //cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-Truthear-ZERO-Drop-Banner-01.jpg?v=1675945400&amp;width=1000 1000w"
+            width="1180"
+            height="700"
+            loading="lazy"
+            sizes="(max-width: 699px) 100vw, min(780px, 50vw)"
           />
-          <div className="w-full h-full absolute top-0  text-white  text-2xl md:text-4xl font-bold  py-10 ">
-            <p className="wifull h-full  mx-10 flex justify-center md:justify-start items-end  text-center  my-auto">
-              Truthear Zero
-            </p>
+          <div className=" absolute bottom-10 left-10 text-white  text-xl sm:text-2xl md:text-3xl lg:text-4xlfont-bold  ">
+            <p className="">Truthear Zero</p>
           </div>
         </div>
       </div>
