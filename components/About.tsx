fc2d3b41e0628ec21653d94dc9e35294fc5bc23c
Sumit Kumar <itssumit258@gmail.com>
2023-06-05 15:56:57 +0530

    feat. content for about page

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <main className="w-screen h-full flex justify-center text-center">
+      <div className="flex flex-col justify-start items-center p-10 lg:p-28 gap-5">
+        <div className="text-2xl">Hey! Glad to See You Here.</div>
+        <div className="flex flex-col text-lg tracking-wider">
+          <p>This E-commerce project is made for educational purpose only. </p>
+          <p>
+            Any user activities or orders made here is only demonstrating the
+            general flow of an E-commerce platforms.
+          </p>
+          <p>
+            The payment gateway used here is running in a test environment and
+            will not debit any actual money.
+          </p>
+        </div>
+        <div className="font-bold text-lg"> Connect with Me:</div>
+        <div className="flex justify-start gap-2 text-blue-700 underline ">
+          <Link
+            href="https://linkedin.com/in/sumitssr"
+            className="hover:text-purple-700"
+          >
+            LinkedIn
+          </Link>
+          <Link
+            href="https://twitter.com/risesumit"
+            className="hover:text-purple-700"
+          >
+            Twitter
+          </Link>
+          <Link
+            href="https://github.com/sumitkcs"
+            className="hover:text-purple-700"
+          >
+            Github
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default About
+export default About;
