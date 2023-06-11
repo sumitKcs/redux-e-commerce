43821fc1fb1912b0e30535bd10f9c15355c29e4c
Sumit Kumar <itssumit258@gmail.com>
2023-06-11 13:42:36 +0530

    removed extra codes

--- components/PeopleGrid.tsx
+++ components/PeopleGrid.tsx
@@ -1,21 +1,24 @@
-"use client"
+"use client";
 
 import { useGetPeopleGridQuery } from "@/store/apiSlice";
 import React from "react";
 
 const PeopleGrid = () => {
-  const {data: people} = useGetPeopleGridQuery()
+  const { data: people } = useGetPeopleGridQuery();
   return (
     <section className="container mx-auto mb-5 lg:mb-0 grid grid-cols-2 people-grid md:scale-y-[.85] md:scale-x-95 scursor-pointer gap-3 md:gap-4 lg:gap-5 mt-10">
       {/* cell 1  */}
 
-      {people?.map(person => (
- <div className=" flex  person drop-shadow-xl overflow-hidden transition-transform duration-200 ease-out rounded-2xl border border-black" key={person.id}>
- <img
-   className="w-full h-fullrounded-2xl object-cover zoom-in"
-   src={`${person?.person_image}width=1500`}
-   alt=""
-   srcSet={`
+      {people?.map((person) => (
+        <div
+          className=" flex  person drop-shadow-xl overflow-hidden transition-transform duration-200 ease-out rounded-2xl "
+          key={person.id}
+        >
+          <img
+            className="w-full h-fullrounded-2xl object-cover zoom-in"
+            src={`${person?.person_image}width=1500`}
+            alt=""
+            srcSet={`
             ${person?.person_image}width=1500,
             ${person?.person_image}width=200 200w,
             ${person?.person_image}width=300 300w
@@ -28,83 +31,13 @@ const PeopleGrid = () => {
             ${person?.person_image}width=1200 1200w
             ${person?.person_image}width=1400 1400w
             `}
-   width="1500"
-   height="1500"
-   loading="lazy"
-   sizes="(max-width: 699px) 100vw, min(390px, 25vw)"
- />
-</div>
+            width="1500"
+            height="1500"
+            loading="lazy"
+            sizes="(max-width: 699px) 100vw, min(390px, 25vw)"
+          />
+        </div>
       ))}
-     
-
-      {/* cell 2  */}
-      {/* <div className="flex justify-center people-2 drop-shadow-xl overflow-hidden transition-transform duration-200 ease-out rounded-2xl ">
-        <img
-          className="w-full rounded-2xl object-cover zoom-in"
-          src="//cdn.shopify.com/s/files/1/0153/8863/files/2_53fad944-fb2c-45d9-97eb-f7376f499b58.jpg?v=1676882683&amp;width=1500"
-          alt=""
-          srcSet="//cdn.shopify.com/s/files/1/0153/8863/files/2_53fad944-fb2c-45d9-97eb-f7376f499b58.jpg?v=1676882683&amp;width=200 200w, //cdn.shopify.com/s/files/1/0153/8863/files/2_53fad944-fb2c-45d9-97eb-f7376f499b58.jpg?v=1676882683&amp;width=300 300w, //cdn.shopify.com/s/files/1/0153/8863/files/2_53fad944-fb2c-45d9-97eb-f7376f499b58.jpg?v=1676882683&amp;width=400 400w, //cdn.shopify.com/s/files/1/0153/8863/files/2_53fad944-fb2c-45d9-97eb-f7376f499b58.jpg?v=1676882683&amp;width=500 500w, //cdn.shopify.com/s/files/1/0153/8863/files/2_53fad944-fb2c-45d9-97eb-f7376f499b58.jpg?v=1676882683&amp;width=600 600w, //cdn.shopify.com/s/files/1/0153/8863/files/2_53fad944-fb2c-45d9-97eb-f7376f499b58.jpg?v=1676882683&amp;width=700 700w, //cdn.shopify.com/s/files/1/0153/8863/files/2_53fad944-fb2c-45d9-97eb-f7376f499b58.jpg?v=1676882683&amp;width=800 800w, //cdn.shopify.com/s/files/1/0153/8863/files/2_53fad944-fb2c-45d9-97eb-f7376f499b58.jpg?v=1676882683&amp;width=1000 1000w, //cdn.shopify.com/s/files/1/0153/8863/files/2_53fad944-fb2c-45d9-97eb-f7376f499b58.jpg?v=1676882683&amp;width=1200 1200w, //cdn.shopify.com/s/files/1/0153/8863/files/2_53fad944-fb2c-45d9-97eb-f7376f499b58.jpg?v=1676882683&amp;width=1400 1400w"
-          width="1500"
-          height="1500"
-          loading="lazy"
-          sizes="(max-width: 699px) 100vw, min(390px, 25vw)"
-        />
-      </div> */}
-
-      {/* cell 3  */}
-      {/* <div className="flex justify-center people-3 drop-shadow-xl overflow-hidden transition-transform duration-200 ease-out rounded-2xl ">
-        <img
-          className="w-full rounded-2xl object-cover zoom-in"
-          src="//cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=3000"
-          alt=""
-          srcSet="//cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=200 200w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=300 300w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=400 400w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=500 500w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=600 600w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=700 700w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=800 800w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=1000 1000w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=1200 1200w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=1400 1400w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=1600 1600w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=1800 1800w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=2000 2000w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=2200 2200w, //cdn.shopify.com/s/files/1/0153/8863/files/3_9d88e8c4-7b6b-4174-bbc7-81dc1c5eb95e.jpg?v=1676881502&amp;width=2400 2400w"
-          width="3000"
-          height="1500"
-          loading="lazy"
-          sizes="(max-width: 699px) 100vw, min(780px, 50vw)"
-        ></img>
-      </div> */}
-
-      {/* cell 4 */}
-      {/* <div className="flex justify-center people-4 drop-shadow-xl overflow-hidden transition-transform duration-200 ease-out rounded-2xl ">
-        <img
-          className="w-full rounded-2xl object-cover zoom-in"
-          src="//cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=3000"
-          alt=""
-          srcSet="//cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=200 200w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=300 300w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=400 400w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=500 500w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=600 600w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=700 700w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=800 800w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=1000 1000w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=1200 1200w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=1400 1400w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=1600 1600w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=1800 1800w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=2000 2000w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=2200 2200w, //cdn.shopify.com/s/files/1/0153/8863/files/4_3d8aec33-0c30-48b5-952d-1f58934f49a0.jpg?v=1676882012&amp;width=2400 2400w"
-          width="3000"
-          height="1500"
-          loading="lazy"
-          sizes="(max-width: 699px) 100vw, min(780px, 50vw)"
-        ></img>
-      </div> */}
-
-      {/* cell 5  */}
-      {/* <div className="flex justify-center people-5 drop-shadow-xl overflow-hidden transition-transform duration-200 ease-out rounded-2xl ">
-        <img
-          className="w-full rounded-2xl object-cover zoom-in"
-          src="//cdn.shopify.com/s/files/1/0153/8863/files/7_7f4f7654-d599-4e7c-8772-607a021d5f22.jpg?v=1676887066&amp;width=1500"
-          alt=""
-          srcSet="//cdn.shopify.com/s/files/1/0153/8863/files/7_7f4f7654-d599-4e7c-8772-607a021d5f22.jpg?v=1676887066&amp;width=200 200w, //cdn.shopify.com/s/files/1/0153/8863/files/7_7f4f7654-d599-4e7c-8772-607a021d5f22.jpg?v=1676887066&amp;width=300 300w, //cdn.shopify.com/s/files/1/0153/8863/files/7_7f4f7654-d599-4e7c-8772-607a021d5f22.jpg?v=1676887066&amp;width=400 400w, //cdn.shopify.com/s/files/1/0153/8863/files/7_7f4f7654-d599-4e7c-8772-607a021d5f22.jpg?v=1676887066&amp;width=500 500w, //cdn.shopify.com/s/files/1/0153/8863/files/7_7f4f7654-d599-4e7c-8772-607a021d5f22.jpg?v=1676887066&amp;width=600 600w, //cdn.shopify.com/s/files/1/0153/8863/files/7_7f4f7654-d599-4e7c-8772-607a021d5f22.jpg?v=1676887066&amp;width=700 700w, //cdn.shopify.com/s/files/1/0153/8863/files/7_7f4f7654-d599-4e7c-8772-607a021d5f22.jpg?v=1676887066&amp;width=800 800w, //cdn.shopify.com/s/files/1/0153/8863/files/7_7f4f7654-d599-4e7c-8772-607a021d5f22.jpg?v=1676887066&amp;width=1000 1000w, //cdn.shopify.com/s/files/1/0153/8863/files/7_7f4f7654-d599-4e7c-8772-607a021d5f22.jpg?v=1676887066&amp;width=1200 1200w, //cdn.shopify.com/s/files/1/0153/8863/files/7_7f4f7654-d599-4e7c-8772-607a021d5f22.jpg?v=1676887066&amp;width=1400 1400w"
-          width="1500"
-          height="1499"
-          loading="lazy"
-          sizes="(max-width: 699px) 100vw, min(390px, 25vw)"
-        />
-      </div> */}
-      {/* cell 6  */}
-      {/* <div className="people-6 drop-shadow-xl overflow-hidden transition-transform duration-200 ease-out rounded-2xl ">
-        <img
-          className="object-cover rounded-2xl zoom-in"
-          src="//cdn.shopify.com/s/files/1/0153/8863/files/5_20a2b9ca-4806-47ed-9cea-9dac0d251aa2.jpg?v=1676883208&amp;width=1500"
-          alt=""
-          srcSet="//cdn.shopify.com/s/files/1/0153/8863/files/5_20a2b9ca-4806-47ed-9cea-9dac0d251aa2.jpg?v=1676883208&amp;width=200 200w, //cdn.shopify.com/s/files/1/0153/8863/files/5_20a2b9ca-4806-47ed-9cea-9dac0d251aa2.jpg?v=1676883208&amp;width=300 300w, //cdn.shopify.com/s/files/1/0153/8863/files/5_20a2b9ca-4806-47ed-9cea-9dac0d251aa2.jpg?v=1676883208&amp;width=400 400w, //cdn.shopify.com/s/files/1/0153/8863/files/5_20a2b9ca-4806-47ed-9cea-9dac0d251aa2.jpg?v=1676883208&amp;width=500 500w, //cdn.shopify.com/s/files/1/0153/8863/files/5_20a2b9ca-4806-47ed-9cea-9dac0d251aa2.jpg?v=1676883208&amp;width=600 600w, //cdn.shopify.com/s/files/1/0153/8863/files/5_20a2b9ca-4806-47ed-9cea-9dac0d251aa2.jpg?v=1676883208&amp;width=700 700w, //cdn.shopify.com/s/files/1/0153/8863/files/5_20a2b9ca-4806-47ed-9cea-9dac0d251aa2.jpg?v=1676883208&amp;width=800 800w, //cdn.shopify.com/s/files/1/0153/8863/files/5_20a2b9ca-4806-47ed-9cea-9dac0d251aa2.jpg?v=1676883208&amp;width=1000 1000w, //cdn.shopify.com/s/files/1/0153/8863/files/5_20a2b9ca-4806-47ed-9cea-9dac0d251aa2.jpg?v=1676883208&amp;width=1200 1200w, //cdn.shopify.com/s/files/1/0153/8863/files/5_20a2b9ca-4806-47ed-9cea-9dac0d251aa2.jpg?v=1676883208&amp;width=1400 1400w"
-          width="1500"
-          height="1500"
-          loading="lazy"
-          sizes="(max-width: 699px) 100vw, min(390px, 25vw)"
-        />
-      </div> */}
     </section>
   );
 };
