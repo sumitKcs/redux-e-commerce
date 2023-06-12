76ae0acb0cd51835c74dbbbe7ecf7c9d2109d282
Sumit Kumar <itssumit258@gmail.com>
2023-06-12 18:28:18 +0530

    fix: product grid small image replacement

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -6,7 +6,7 @@ import React, { useState } from "react";
 
 const ProductGrid = () => {
   const { data: products } = useGetProductGridQuery();
-  const [isLoaded, setIsLoaded] = useState(false);
+  console.log("product grid", products);
   return (
     <section className="container h-[90svh]  mx-auto mb-5 grid grid-cols-2 product-grid md:scale-y-[.80] md:scale-x-95 cursor-pointer gap-3 p-4 md:px-10 ">
       {products
@@ -18,27 +18,24 @@ const ProductGrid = () => {
             >
               <div className="w-full h-full background ">
                 <img
-                  className={`${
-                    !isLoaded ? "hidden" : "block animate"
-                  } w-full h-full rounded-2xl object-cover zoom-in`}
-                  src={`${product?.grid_image}width=1500`}
+                  className={`animate w-full h-full rounded-2xl object-cover zoom-in`}
+                  src={`https:${product?.grid_image}width=1500`}
                   alt=""
                   srcSet={`
-            ${product?.grid_image}width=1500,
-            ${product?.grid_image}width=200 200w,
-            ${product?.grid_image}width=300 300w
-            ${product?.grid_image}width=400 400w
-            ${product?.grid_image}width=500 500w
-            ${product?.grid_image}width=600 600w
-            ${product?.grid_image}width=700 700w
-            ${product?.grid_image}width=800 800w
-            ${product?.grid_image}width=1000 1000w
-            ${product?.grid_image}width=1200 1200w
-            ${product?.grid_image}width=1400 1400w
+            https:${product?.grid_image}width=1500,
+            https:${product?.grid_image}width=200 200w,
+            https:${product?.grid_image}width=300 300w
+            https:${product?.grid_image}width=400 400w
+            https:${product?.grid_image}width=500 500w
+            https:${product?.grid_image}width=600 600w
+            https:${product?.grid_image}width=700 700w
+            https:${product?.grid_image}width=800 800w
+            https:${product?.grid_image}width=1000 1000w
+            https:${product?.grid_image}width=1200 1200w
+            https:${product?.grid_image}width=1400 1400w
             `}
                   loading="lazy"
                   sizes="(max-width: 699px) 100vw, min(780px, 50vw)"
-                  onLoad={() => setIsLoaded(true)}
                 />
               </div>
               <div className="absolute top-0  text-white  text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold ">
