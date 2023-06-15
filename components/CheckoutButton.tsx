88f78449b59d61ffa5b2bf7f6dfc21ca34020004
Sumit Kumar <itssumit258@gmail.com>
2023-06-15 17:57:59 +0530

    fix: removed updateCartDataToFirestore import

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -8,7 +8,6 @@ import { useRouter } from "next/navigation";
 import { loadStripe } from "@stripe/stripe-js";
 import { useSession } from "next-auth/react";
 import axios from "axios";
-import updateCartDataToFirestore from "@/lib/updateCartDataToFirestore";
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
