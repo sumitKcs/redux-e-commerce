eae422e1b0e1fdae155258c141e40d121c57ca3b
Sumit Kumar <itssumit258@gmail.com>
2023-06-18 16:26:47 +0530

    fix: https image urls

--- components/ImageBanner.tsx
+++ components/ImageBanner.tsx
@@ -9,23 +9,23 @@ const ImageBanner = ({ desktopImage, mobileImage, text }: Props) => {
       {/* desktop image */}
       <img
         className="w-full object-cover h-full lg:h-auto hidden md:block ease-in duration-700"
-        src={`${desktopImage}&width=3200`}
+        src={`https:${desktopImage}&width=3200`}
         alt={"heaphone banner"}
         srcSet={`
-              ${desktopImage}&width=200 200w,
-              ${desktopImage}&width=300 300w,
-              ${desktopImage}&width=400 400w,
-              ${desktopImage}&width=500 500w,
-              ${desktopImage}&width=600 600w,
-              ${desktopImage}&width=700 700w,
-              ${desktopImage}&width=800 800w,
-              ${desktopImage}&width=900 900w,
-              ${desktopImage}&width=1000 1000w,
-              ${desktopImage}&width=1200 1200w,
-              ${desktopImage}&width=1400 1400w,
-              ${desktopImage}&width=1600 1600w,
-              ${desktopImage}&width=1800 1800w,
-              ${desktopImage}&width=2000 2000w,
+              https:${desktopImage}&width=200 200w,
+              https:${desktopImage}&width=300 300w,
+              https:${desktopImage}&width=400 400w,
+              https:${desktopImage}&width=500 500w,
+              https:${desktopImage}&width=600 600w,
+              https:${desktopImage}&width=700 700w,
+              https:${desktopImage}&width=800 800w,
+              https:${desktopImage}&width=900 900w,
+              https:${desktopImage}&width=1000 1000w,
+              https:${desktopImage}&width=1200 1200w,
+              https:${desktopImage}&width=1400 1400w,
+              https:${desktopImage}&width=1600 1600w,
+              https:${desktopImage}&width=1800 1800w,
+              https:${desktopImage}&width=2000 2000w,
               
               `}
         width="2160"
@@ -37,19 +37,19 @@ const ImageBanner = ({ desktopImage, mobileImage, text }: Props) => {
       {/* mobile image  */}
       <img
         className="w-full object-cover h-full lg:h-auto md:hidden ease-in duration-700"
-        src={`${mobileImage}&width=1300`}
+        src={`https:${mobileImage}&width=1300`}
         alt={"headphone banner"}
         srcSet={`
-              ${mobileImage}&width=200 200w,
-              ${mobileImage}&width=300 300w,
-              ${mobileImage}&width=400 400w,
-              ${mobileImage}&width=500 500w,
-              ${mobileImage}&width=600 600w,
-              ${mobileImage}&width=700 700w,
-              ${mobileImage}&width=800 800w,
-              ${mobileImage}&width=900 900w,
-              ${mobileImage}&width=1000 1000w,
-              ${mobileImage}&width=1200 1200w,
+              https:${mobileImage}&width=200 200w,
+              https:${mobileImage}&width=300 300w,
+              https:${mobileImage}&width=400 400w,
+              https:${mobileImage}&width=500 500w,
+              https:${mobileImage}&width=600 600w,
+              https:${mobileImage}&width=700 700w,
+              https:${mobileImage}&width=800 800w,
+              https:${mobileImage}&width=900 900w,
+              https:${mobileImage}&width=1000 1000w,
+              https:${mobileImage}&width=1200 1200w,
               
               `}
         width="1200"
