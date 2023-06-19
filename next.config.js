6c99b9a770e8b66940c7eb29db72785e98694d98
Sumit Kumar <itssumit258@gmail.com>
2023-06-19 13:10:11 +0530

    fix: removed unused files

--- next.config.js
+++ next.config.js
@@ -4,13 +4,14 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: [
-      "fakestoreapi.com",
-      "lh3.googleusercontent.com",
-      "cdn.shopify.com",
-    ],
+    domains: ["lh3.googleusercontent.com", "cdn.shopify.com"],
   },
   // webpack5: false,
 };
 
 module.exports = nextConfig;
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+module.exports = withBundleAnalyzer({});
